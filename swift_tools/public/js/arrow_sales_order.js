f4d8d2e1de4d394d2437c2a7c7389927e9889b0b
msabour11 <mohamedabdo2611@gmail.com>
2026-04-23 16:46:26 +0200

    add filter script

---
+++ swift_tools/public/js/arrow_sales_order.js
@@ -0,0 +1,20 @@
+frappe.ui.form.on("Sales Order", {
+	refresh: function (frm) {
+		frm.set_query("item_code", "items", function () {
+			return {
+				filters: {
+					custom_item_type: frm.doc.custom_item_type,
+				},
+			};
+		});
+	},
+	custom_item_type: function (frm) {
+		frm.set_query("item_code", "items", function () {
+			return {
+				filters: {
+					custom_item_type: frm.doc.custom_item_type,
+				},
+			};
+		});
+	},
+});
