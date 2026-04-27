bec91518025c70ef2230b383b3804f47c4bdda19
msabour11 <mohamedabdo2611@gmail.com>
2026-04-27 13:45:37 +0300

    add depends property

--- swift_tools/hooks.py
+++ swift_tools/hooks.py
@@ -11,6 +11,7 @@ app_license = "mit"
 required_apps = ["frappe", "erpnext"]
 fixtures = [
     {"dt": "Custom Field", "filters": [["module", "=", "Swift Tools"]]},
+    {"dt": "Property Setter", "filters": [["module", "=", "Swift Tools"]]},
 ]
 after_install = "swift_tools.setup.install.after_install"
 doctype_js = {
