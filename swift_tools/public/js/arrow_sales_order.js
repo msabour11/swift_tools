4171bcd7a096880e70ad5006b49254293f30d15a
msabour11 <mohamedabdo2611@gmail.com>
2026-06-10 17:48:26 +0300

    add other charges

--- swift_tools/public/js/arrow_sales_order.js
+++ swift_tools/public/js/arrow_sales_order.js
@@ -49,3 +49,23 @@ frappe.ui.form.on("Sales Order", {
 		});
 	},
 });
+
+frappe.ui.form.on("Sales Order Item", {
+	custom_trans_charge: function (frm, cdt, cdn) {
+		calculate_rate(frm, cdt, cdn);
+	},
+	custom_other_charge: function (frm, cdt, cdn) {
+		calculate_rate(frm, cdt, cdn);
+	},
+});
+
+function calculate_rate(frm, cdt, cdn) {
+	var row = locals[cdt][cdn];
+	let trans_charge = flt(row.custom_trans_charge);
+	let other_charge = flt(row.custom_other_charge);
+	let new_rate = trans_charge + other_charge;
+
+	frappe.model.set_value(cdt, cdn, "rate", new_rate, () => {
+		frm.refresh_field("items");
+	});
+}
