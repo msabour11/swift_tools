dac727e3848dd8ea69f24e266e04799cfd61541c
msabour11 <mohamedabdo2611@gmail.com>
2026-06-10 16:34:22 +0300

    add po from so

--- swift_tools/public/js/arrow_sales_order.js
+++ swift_tools/public/js/arrow_sales_order.js
@@ -3,16 +3,47 @@ frappe.ui.form.on("Sales Order", {
 		frm.set_query("item_code", "items", function () {
 			return {
 				filters: {
-					custom_item_type: frm.doc.custom_item_type,
+					custom_item_type: frm.doc.item_type,
 				},
 			};
 		});
+
+		if (!frm.is_new()) {
+			frm.add_custom_button(
+				__("Purchase Order"),
+				function () {
+					frappe.model.with_doctype("Purchase Order", function () {
+						let po = frappe.model.get_new_doc("Purchase Order");
+
+						if (frm.doc.custom_car_owner) {
+							po.supplier = frm.doc.custom_car_owner;
+						}
+
+						(frm.doc.items || []).forEach(function (row) {
+							let child = frappe.model.add_child(po, "items");
+							child.item_code = row.item_code;
+							child.item_name = row.item_name;
+							child.description = row.description;
+							child.qty = row.qty;
+							child.uom = row.uom;
+							child.schedule_date =
+								row.delivery_date ||
+								frm.doc.delivery_date ||
+								frappe.datetime.get_today();
+						});
+
+						frappe.set_route("Form", "Purchase Order", po.name);
+					});
+				},
+				__("Create"),
+			);
+		}
 	},
-	custom_item_type: function (frm) {
+	item_type: function (frm) {
 		frm.set_query("item_code", "items", function () {
 			return {
 				filters: {
-					custom_item_type: frm.doc.custom_item_type,
+					custom_item_type: frm.doc.item_type,
 				},
 			};
 		});
